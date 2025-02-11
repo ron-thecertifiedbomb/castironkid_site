b73359026728d9873a789a1e130e18a61e4a11db
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-11 14:47:34 +0800

    created useAuthenticateHook

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -104,27 +104,29 @@ const Food = () => {
 
       {/* Display foods */}
       <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
-        {foods.map((item, index) => (
-          <div
-            key={index}
-            className='border shadow-lg rounded-lg hover:scale-105 duration-300'
-          >
-            <img
-              src={item.image}
-              alt={item.name}
-              className='w-full h-[200px] object-cover rounded-t-lg'
-            />
-            <div className='flex justify-between px-2 py-4'>
-              <p className='font-bold'>{item.name}</p>
-              <p>
-                <span className='bg-orange-500 text-white p-1 rounded-full'>
-                  {item.price}
-                </span>
-              </p>
-            </div>
-          </div>
-        ))}
+  {foods.map((item, index) => (
+    <div
+      key={index}
+      className='border shadow-lg rounded-lg hover:scale-105 duration-300 cursor-pointer'
+      onClick={() => console.log(item)} // Log item when clicked
+    >
+      <img
+        src={item.image}
+        alt={item.name}
+        className='w-full h-[200px] object-cover rounded-t-lg'
+      />
+      <div className='flex justify-between px-2 py-4'>
+        <p className='font-bold'>{item.name}</p>
+        <p>
+          <span className='bg-orange-500 text-white p-1 rounded-full'>
+            {item.price}
+          </span>
+        </p>
       </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 };
